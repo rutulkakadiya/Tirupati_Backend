28fda30f1438d71d697e9bfc7e8d1a82c72d2c3b
rutulkakadiya <rutulkakadiya973@gmail.com>
2025-05-18 15:16:08 +0530

    Tirupati_backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const UserModel = require('../model/UserModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 module.exports.register = async (req, res) => {

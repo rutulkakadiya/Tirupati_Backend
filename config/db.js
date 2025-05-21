bee568fb9a8cbab23ee53e2764439de5e71e8e53
rutulkakadiya <rutulkakadiya973@gmail.com>
2025-05-21 06:30:21 +0530

    Tirupati

--- config/db.js
+++ config/db.js
@@ -12,4 +12,15 @@ const sequelize = new Sequelize(
   }
 );
 
+module.exports = {
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};
+
+
 module.exports = { sequelize };
